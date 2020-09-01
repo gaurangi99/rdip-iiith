9e66e783eb2d17984aa57fad87ca15efed4a0a27
gaurangi99 <imgaurangi.99@gmail.com>
2020-09-01 22:02:58 +0530

    added folder for js

---
+++ js-files/basics.js
@@ -0,0 +1,33 @@
+console.log('Hello World!');
+
+console.log('35'+'24');
+console.log(35+24);
+console.log(35-24);
+console.log(35*24);
+console.log(35/24);
+console.log(35%24);
+
+let cheeku=18;
+console.log(cheeku);
+cheeku=17.5;
+console.log(cheeku);
+var gul="gaurangi";
+console.log(gul);
+
+if(gul==='Gaurangi')
+console.log(true);
+else{
+console.log(false);
+}
+
+console.log(multiply(2,98));
+function multiply(a,b){
+    var c=a*b;
+    return c;
+}
+console.log(multiply(99,100));
+
+// document.querySelector('html').onclick=function(){
+//     alert('Ouch! Stop poking me!');
+// }
+
