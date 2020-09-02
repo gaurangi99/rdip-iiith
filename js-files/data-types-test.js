11ebb7a0a68e65d24c4d599432cafef57a3f9fe3
gaurangi99 <imgaurangi.99@gmail.com>
2020-09-02 17:28:51 +0530

    added datatypes file

---
+++ js-files/data-types-test.js
@@ -0,0 +1,39 @@
+var length=89;
+console.log(length);
+
+var firstName='Gaurangi';
+console.log(firstName);
+
+var s={firstname:"gaurangi",lastname:"agrawal"};
+console.log(s);
+
+var t=26+"November";
+console.log(t);
+
+var x=9;
+console.log(x);
+x='johny';
+console.log(x);
+
+var y=1234e10;
+console.log(y);
+
+var m=99;
+let n=100;
+console.log(m===n);
+
+var cars=['baleno', 'swift', 's-cross'];
+console.log(cars[2]);
+
+var person={firstname:'roshan', lastname:'singh', age:9, salary:'8.5'};
+console.log(person.salary);
+
+console.log(typeof '');
+console.log(typeof x);
+console.log(typeof person);
+console.log(typeof y);
+console.log(typeof null);
+
+person=null;
+console.log(person);
+
