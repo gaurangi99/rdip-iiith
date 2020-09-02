891a3a2de33065495b4a8b019670f469922d7e55
gaurangi99 <imgaurangi.99@gmail.com>
2020-09-02 16:58:06 +0530

    added variables file

---
+++ js-files/variables-test.js
@@ -0,0 +1,17 @@
+let x=5;
+const y=9;
+x=10;
+var z=x+y;
+console.log(z);
+
+var m;
+console.log(m);
+
+var s = "Gaurangi" + " " + "Agrawal";
+console.log(s);
+
+var t=5+10+"8";
+var u="5"+'2'+2;
+console.log(t);
+console.log(u);
+
