a4ccdc6b824461e1fc8552353855822496e0bc7d
gaurangi99 <imgaurangi.99@gmail.com>
2020-09-02 17:44:25 +0530

    added operators file

---
+++ js-files/operators-test.js
@@ -0,0 +1,24 @@
+var x=10;
+var y=11;
+var z=x**y;
+console.log(z);
+
+var m=5;
+var n=5;
+var o='5';
+console.log(m==n);
+console.log(m===o);
+console.log(m==o);
+
+console.log(m!=n);
+console.log(m!==o);
+console.log(m!=o);
+
+console.log(5 & 1);
+console.log(5 | 1);
+console.log(~ 5);
+console.log(5 ^ 1);
+console.log(5 << 1);
+console.log(5 >> 1);
+console.log(5 >>> 1);
+console.log();
