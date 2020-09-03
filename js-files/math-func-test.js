216c2466c7b2b7734209d00af7c3f51834c2bdba
gaurangi99 <imgaurangi.99@gmail.com>
2020-09-03 07:09:33 +0530

    added math functions file

---
+++ js-files/math-func-test.js
@@ -0,0 +1,13 @@
+console.log(Math.pow(10,10));
+console.log(Math.sqrt(216));
+console.log(Math.abs(-40.22));
+
+var randNum=11*Math.random();
+console.log(randNum);
+console.log(Math.round(randNum));
+console.log(Math.ceil(randNum));
+console.log(Math.floor(randNum));
+
+console.log(Math.sin(Math.PI));
+console.log(Math.cos(Math.PI/2));
+console.log(Math.tan(Math.PI/4));
