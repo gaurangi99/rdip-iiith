40b8c25f6354d27fbbbefd4224e46f1ae4e368bb
gaurangi99 <imgaurangi.99@gmail.com>
2020-09-05 06:12:14 +0530

    added error handling

--- js-files/control-flows-test.js
+++ js-files/control-flows-test.js
@@ -1,43 +1,78 @@
-function numCheckByIf(num){
-    if(num>10 && num<100){
-        console.log("Between 10-100.");
-    }
-    else{
-        console.log("Not in Between 10-100.");
-    }
-}
-
-function numCheckBySwitch(num){
-    var q=Math.floor(num/10);
-    switch(q){
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:console.log("Between 10-100.");
-                break;
-        default:console.log("Not in Between 10-100.");
-                break;
+try{
+    function numCheckByIf(num){
+        if(isNaN(num)){
+            throw (new Error('Enter number!!'));
+        }
+        if(num>10 && num<100){
+            console.log("Between 10-100.");
+        }
+        else{
+            console.log("Not in Between 10-100.");
+        }
     }
 }
+catch(e){
+    console.log(e.name);
+    console.log(e.message);
+}
 
-function fib(elements){
-    if(elements==1){
-        return 0;
+try{
+    function numCheckBySwitch(num){
+        if(isNaN(num)){
+            throw (new Error('ONLY Enter number!!'));
+        }
+        var q=Math.floor(num/10);
+        switch(q){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:console.log("Between 10-100.");
+                    break;
+            default:console.log("Not in Between 10-100.");
+                    break;
+        }
     }
-    else if(elements==2){
-        return 1;
+}
+catch(er){
+    console.log(er.name);
+    console.log(er.message);
+}
+finally{   
+    numCheckBySwitch(325);
+}
+
+try{
+    function fib(elements){
+        if(num<1){
+            throw (new Error('Enter number greater than 0!'));
+        }
+        if(elements==1){
+            return 0;
+        }
+        else if(elements==2){
+            return 1;
+        }
+        else{
+            var fibNum=fib(elements-1)+fib(elements-2);
+            return fibNum;
+        }
     }
-    else{
-        var fibNum=fib(elements-1)+fib(elements-2);
-        return fibNum;
-    }
+}
+catch(err){
+    console.log(err.name);
+    console.log(err.message);
+}
+finally{
+    console.log('Errors in fibonacci recorded.');
 }
 
 numCheckByIf(65);
-numCheckBySwitch(325);
+numCheckByIf("cheeku");
+numCheckBySwitch("bubbly");
 console.log(fib(6));
+console.log(fib(-1));
