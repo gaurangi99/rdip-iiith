02f6a23a53462620874640370fde92df9bcb6230
gaurangi99 <imgaurangi.99@gmail.com>
2020-09-05 07:00:15 +0530

    added  string method manipulation

---
+++ js-files/string-test.js
@@ -0,0 +1,9 @@
+function stringSplit(){
+    var str="HTML is the standard markup language for Web pages. With HTML you can create your own Website.";
+    var splitStr=str.split(" ");
+    console.log(splitStr[4]);
+    console.log(splitStr[6]);
+    console.log(splitStr[9]);
+}
+
+console.log(stringSplit());
